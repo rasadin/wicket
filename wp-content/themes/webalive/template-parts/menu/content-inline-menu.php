92d6353954b0af098ecb9ff143fddbb864bc669f
Mrittunjoy <mrittunjoy@bitmascot.com>
2024-03-31 21:44:54 +0600

    design

--- wp-content/themes/webalive/template-parts/menu/content-inline-menu.php
+++ wp-content/themes/webalive/template-parts/menu/content-inline-menu.php
@@ -32,7 +32,7 @@
 							) );
 						endif;
 					?>
-					<div class="right-button"><a href="#">জরুরি যোগাযোগ</a></div>
+					<div class="right-button"><a href="<?php echo esc_url( home_url( '/জরুরি-যোগাযোগ/' ) ); ?>">জরুরি যোগাযোগ</a></div>
 				</div>
 				
 		    </div>
